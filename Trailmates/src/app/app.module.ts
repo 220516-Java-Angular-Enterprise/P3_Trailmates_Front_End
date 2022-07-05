951ac674a0d62d2f817f1ac395a1027081e58b0b
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-07-05 16:14:32 -0500

    finished folder structure.

--- Trailmates/src/app/app.module.ts
+++ Trailmates/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MessagingComponent } from './messaging-page/messaging/messaging.component';
+import { ProfileComponent } from './profile-page/profile/profile.component';
+import { TrailComponent } from './trail-page/trail/trail.component';
+import { LandingComponent } from './auth/landing/landing.component';
+import { LoginComponent } from './auth/login/login.component';
+import { CreateAccComponent } from './auth/create-acc/create-acc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MessagingComponent,
+    ProfileComponent,
+    TrailComponent,
+    LandingComponent,
+    LoginComponent,
+    CreateAccComponent
   ],
   imports: [
     BrowserModule,
