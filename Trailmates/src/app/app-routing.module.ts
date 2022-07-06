9f692c54033bd8cb465ad0aa09585484a6763d12
Ryan Jacobo <ryanjacobo@yahoo.com>
2022-07-06 11:26:06 -0700

    created navbar with styling on header

--- Trailmates/src/app/app-routing.module.ts
+++ Trailmates/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
+import { ProfileComponent } from './profile-page/profile/profile.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
