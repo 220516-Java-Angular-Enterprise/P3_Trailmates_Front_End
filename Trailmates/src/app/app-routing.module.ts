1c0a5b40b1a96b0b77ef6ab8be1685ec974b5be2
victo <victoriatrotter95@gmail.com>
2022-07-06 10:46:06 -0400

    Routing

--- Trailmates/src/app/app-routing.module.ts
+++ Trailmates/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MessagingComponent } from './messaging-page/messaging/messaging.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{path:'/messaging', component: MessagingComponent}
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
