e609189fe2c2665a3873ac13d150b5751bf06efa
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-07-06 11:49:27 -0500

    Finished designing notification, user, and search comp.

--- Trailmates/src/app/app.module.ts
+++ Trailmates/src/app/app.module.ts
@@ -10,6 +10,10 @@ import { TrailComponent } from './trail-page/trail/trail.component';
 import { LandingComponent } from './auth/landing/landing.component';
 import { LoginComponent } from './auth/login/login.component';
 import { CreateAccComponent } from './auth/create-acc/create-acc.component';
+import { NotifiationsComponent } from './header/notifiations/notifiations.component';
+import { SearchAllComponent } from './header/search-all/search-all.component';
+import { SearchTrailsComponent } from './header/search-trails/search-trails.component';
+import { UserMenuComponent } from './header/user-menu/user-menu.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +24,11 @@ import { CreateAccComponent } from './auth/create-acc/create-acc.component';
     TrailComponent,
     LandingComponent,
     LoginComponent,
-    CreateAccComponent
+    CreateAccComponent,
+    NotifiationsComponent,
+    SearchAllComponent,
+    SearchTrailsComponent,
+    UserMenuComponent
   ],
   imports: [
     BrowserModule,
---
+++ Trailmates/src/app/header/notifiations/notifiations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-notifiations',
+  templateUrl: './notifiations.component.html',
+  styleUrls: ['./notifiations.component.scss']
+})
+export class NotifiationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
