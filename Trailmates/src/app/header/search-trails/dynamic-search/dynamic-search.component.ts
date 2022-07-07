b42f05d52708ce01c2198e0950799ce504c80db7
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-07-07 15:19:21 -0500

    Finished search for trail page.

--- Trailmates/src/app/header/search-trails/dynamic-search/dynamic-search.component.ts
+++ Trailmates/src/app/header/search-trails/dynamic-search/dynamic-search.component.ts
@@ -1,5 +1,5 @@
 import { Trail } from 'src/app/models/trail';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 import { TrailService } from 'src/app/services/trail.service';
 
 @Component({
@@ -10,25 +10,16 @@ import { TrailService } from 'src/app/services/trail.service';
 export class DynamicSearchComponent implements OnInit {
 
   constructor(private _trailService: TrailService) { }
-
+  // @Input()
+  // query!: string;
+  @Output() passQuery: EventEmitter<String> = new EventEmitter<String>();
   query: string = '';
-  allTrail: Trail[] = [];
-  filterTrail: Trail[] = [];
 
   ngOnInit(): void {
-    this._trailService.getAllTrails().subscribe((data:any)=>{
-      this.allTrail = data;
-    })
   }
 
-  filterTrails(){
-  this.filterTrail = [];
-  this.allTrail.forEach(trail=>{
-    if(trail.name?.toLocaleLowerCase().includes(this.query.toLocaleLowerCase()) && !this.filterTrail.includes(trail)){
-      this.filterTrail.push(trail);
-    }
-  })
-
+  getQuery(query:string){
+  this.passQuery.emit(query)
   }
 
   
