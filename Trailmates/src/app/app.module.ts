a8a184d19fa9138f6394dcb09eb570f2a205e4a3
Seanghay Prom <97471066+HaibunisCool@users.noreply.github.com>
2022-07-07 13:10:09 -0500

    Update app.module.ts

--- Trailmates/src/app/app.module.ts
+++ Trailmates/src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http'
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -11,7 +10,6 @@ import { TrailComponent } from './trail-page/trail/trail.component';
 import { LandingComponent } from './auth/landing/landing.component';
 import { LoginComponent } from './auth/login/login.component';
 import { CreateAccComponent } from './auth/create-acc/create-acc.component';
-
 import { NotifiationsComponent } from './header/notifications/notifiations.component';
 import { SearchAllComponent } from './header/search-all/search-all.component';
 import { SearchTrailsComponent } from './header/search-trails/search-trails.component';
