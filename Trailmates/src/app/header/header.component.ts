151f0bce21c6610e13cc918799f1f65ee447c278
Ryan Jacobo <ryanjacobo@yahoo.com>
2022-07-07 09:20:04 -0700

    moved styling properties from html to scss

--- Trailmates/src/app/header/header.component.ts
+++ Trailmates/src/app/header/header.component.ts
@@ -3,17 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  showNotifs: boolean = false;
+  showUserMenu: boolean = false;
 
-  showNotifs:boolean = false;
-  showUserMenu:boolean = false;
+  isMenuOpen: boolean = false;
 
-  ngOnInit(): void {
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
 
+
+  ngOnInit(): void {}
 }
