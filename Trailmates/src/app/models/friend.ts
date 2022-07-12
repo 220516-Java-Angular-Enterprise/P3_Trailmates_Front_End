b535eee9d63543dd3308ae75fd0cbb7fff82220a
ML <labrecquemegan@hotmail.com>
2022-07-12 17:45:48 -0400

    added friend service and fixed trail html

---
+++ Trailmates/src/app/models/friend.ts
@@ -0,0 +1,7 @@
+import { User } from "./user";
+
+export interface Friend{
+    id?: string;
+    user_id?: string;
+    friend_id?: string;
+  }
---
+++ Trailmates/src/app/services/friend.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+import { Friend } from '../models/friend';
+import { User } from '../models/user';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FriendService {
+
+  constructor(private http: HttpClient) { }
+  url: string = 'http://trailmates-env.us-east-1.elasticbeanstalk.com/TrailMates/friend/'
+
+addFriend(friend_id: string, friend: Friend) {
+  return this.http.post<any>(this.url + friend_id, friend)
+}
+
+getAllFriends(): Observable<Friend[]>{
+  return this.http.get<Friend[]>(this.url)
+}
+
+getAllPending(): Observable<Friend[]>{
+  return this.http.get<Friend[]>(this.url + 'pending')
+}
+
+}
