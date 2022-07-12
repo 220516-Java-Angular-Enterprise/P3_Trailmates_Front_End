ebef5debcd5dd50dcf78003a37b2c199b06170fd
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-07-12 10:08:22 -0500

    Figured out posting for trail flag.

--- Trailmates/src/app/calendar-modal/calendar-modal.component.ts
+++ Trailmates/src/app/calendar-modal/calendar-modal.component.ts
@@ -1,4 +1,6 @@
-import { NgForm } from '@angular/forms';
+// import { TrailFlagReq } from './../models/dto/trailFlagReq';
+import { TrailFlag } from 'src/app/models/trailFlag';
+import { NgForm, NG_ASYNC_VALIDATORS } from '@angular/forms';
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AnimateTimings } from '@angular/animations';
@@ -16,26 +18,24 @@ export class CalendarModalComponent implements OnInit {
   displayFormSubmitError: boolean = false;
   submitted: boolean = false;
 
-  trailFlagReq ={
+  trailFlagReq= {
     trail_id: '',
     user_id: '',
-    date_int: '',
+    date_int: 0,
   }
   dateInt = new Date().getTime()/(1000*60*60*24); //an integer representing current date as a normalized number of days
-
+  time: string = '00:00'
 
   ngOnInit(): void {
     this.trailFlagReq.trail_id = this.currRoute.firstChild?.snapshot.params['id'];
     this.trailFlagReq.user_id = localStorage.getItem('id')!;
-
-  this._flagService.getFlagById('2750C574-18E4-45F7-B209-740A69A46729').subscribe((data:any) =>{
-  console.log(data);
-});
   }
 
   processForm(flagTrail: NgForm){
     if(flagTrail.form.status == 'VALID'){
-    this._flagService.postTrailFlag(this.trailFlagReq).subscribe((data:any)=>{
+      let date = new Date(this.trailFlagReq.date_int+' '+this.time);
+      this.trailFlagReq.date_int = date.getTime()/(1000*60*60*24);
+      this._flagService.postTrailFlag(this.trailFlagReq).subscribe((data:any)=>{
       console.log(data);
       this.submitted = true;
     })
