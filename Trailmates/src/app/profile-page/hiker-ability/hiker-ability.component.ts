599d92a2277358b62df63b697ff8df27066cfbfc
MrNoms <tiddle96@gmail.com>
2022-07-13 10:41:21 -0700

    Creation of Hiker ability component

---
+++ Trailmates/src/app/profile-page/hiker-ability/hiker-ability.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hiker-ability',
+  templateUrl: './hiker-ability.component.html',
+  styleUrls: ['./hiker-ability.component.scss']
+})
+export class HikerAbilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
