eadc2661ca28f57cc5b89d5dfa7514d5cacbb1ba
mb_q <mbquercioli@gmail.com>
2022-07-14 09:45:07 -0600

    websocket features

--- Trailmates/src/app/messaging-page/messaging/chat-room/chat-room.component.ts
+++ Trailmates/src/app/messaging-page/messaging/chat-room/chat-room.component.ts
@@ -4,6 +4,9 @@ import { Component, Input, OnInit } from '@angular/core';
 import { MessagesService } from 'src/app/services/messages.service';
 import { UserService } from 'src/app/services/user-service.service';
 import { mergeScan } from 'rxjs';
+import * as Stomp from 'stompjs';
+import * as SockJS from 'sockjs-client';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-chat-room',
@@ -11,15 +14,38 @@ import { mergeScan } from 'rxjs';
   styleUrls: ['./chat-room.component.scss']
 })
 export class ChatRoomComponent implements OnInit {
+  
+  // stompClient?: Stomp.Client;
+  id?: string;
+  username?: string;
+  randInt?: string;
+  // newmessage?: string;
+  // greetings: any;
+  
+  greetings: string[] = [];
+  constructor(private _messageService: MessagesService, private userService: UserService, private currRoute: ActivatedRoute) { }
+  ngOnInit(): void {
+    this.currRoute.params.subscribe(p => {
+      this.id = p['id']
+    })
+    this.connect();
+  }
 
-  constructor(private _messageService: MessagesService, private userService: UserService) { }
-
+  newmessage: string | undefined;
+  disabled = true;
+  private stompClient = require('stompjs');
   @Input() convo: Conversation = {};
   privateMessages: PrivateMessage[] = [];
 
-
-  ngOnInit(): void {
+  setConnected(connected: boolean) {
+    this.disabled = !connected;
+    if (connected) {
+      this.greetings = [];
+    }
   }
+
+  // ngOnInit(): void {
+  // }
 
   getPrivateMessages(){
     this._messageService.getPrivateMessagesByConvoName(this.convo.id!).subscribe(
@@ -33,8 +59,11 @@ export class ChatRoomComponent implements OnInit {
 
   consoleConvo(){
     console.log(this.convo);
-    this.getPrivateMessages()
+    // this.getPrivateMessages()
     console.log(this.privateMessages);
+  }
+  onKeydown(event: any){
+    event.preventDefault();
   }
 
   message ='';
@@ -59,7 +88,8 @@ export class ChatRoomComponent implements OnInit {
 
     
     this.postNewMessage(messageRequest)
-    this.getPrivateMessages()
+    // this.getPrivateMessages()
+    this.message = '';
     // this.privateMessages.push(privateMessage)
   }
 
@@ -71,5 +101,38 @@ export class ChatRoomComponent implements OnInit {
       },
       err => console.log(err)
     )
+  }
+
+
+  connect() {
+    const socket = new SockJS('http://localhost:5000/TrailMates/testchat/');
+    this.stompClient = Stomp.over(socket);
+    const _this = this;
+    this.stompClient.connect({}, function (frame: string) {
+      console.log('Connected: ' + frame);
+      _this.stompClient.subscribe('/start/initial', function( hello: {body: string}){
+        _this.showMessage(JSON.parse(hello.body));
+      });
+    });
+  }
+  
+  sendMessage() {
+    this.submitMessage();
+    this.userService.getUserById(localStorage.getItem('id')!).subscribe(
+      data => data.username = this.username
+    )
+    console.log(this.username)
+    this.stompClient.send(
+      '/current/resume',
+      {},
+      //this.obj
+      JSON.stringify(this.id + "~" + this.username + ": " + this.newmessage)
+    );
+    this.newmessage = "";
+  }
+  
+  showMessage(message: string) {
+      var split = message.split("~");
+      if (split[0] == this.id) { this.greetings.push(split[1]); } 
   }
 }
