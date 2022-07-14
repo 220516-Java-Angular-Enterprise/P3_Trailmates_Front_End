a152d1821b72200b590f3e31d40afbe1f62647bf
Joseph Choi <joseph375@revature.net>
2022-07-14 16:06:40 -0400

    Initial implementation of image upload.

--- Trailmates/src/app/services/trail-history.service.ts
+++ Trailmates/src/app/services/trail-history.service.ts
@@ -9,6 +9,7 @@ import { TrailHistory } from '../models/trailHistory';
 })
 export class TrailHistoryService {
   private URL="http://trailmates-env.us-east-1.elasticbeanstalk.com/TrailMates/history";
+  private imageURL = "http://trailmates-env.us-east-1.elasticbeanstalk.com/TrailMates/image/gen-url/";
 
   constructor(private http:HttpClient, private route:Router) { }
 
@@ -18,6 +19,14 @@ export class TrailHistoryService {
 
   getHistoryAsc(): Observable<TrailHistory[]> {
     return this.http.get<TrailHistory[]>(this.URL+'/asc');
+  }
+
+  getSecureURL(fileExtension: String): Promise<String> {
+    return firstValueFrom(this.http.get<String>(this.imageURL + fileExtension));
+  }
+
+  uploadImage(bucketURL: string, uploadedImage: File) {
+    return this.http.put<File>(bucketURL, uploadedImage);
   }
 
   insertNewHistory(newHistory: TrailHistory) {
