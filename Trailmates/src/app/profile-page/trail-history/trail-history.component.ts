503edee654aa7ca8e63cc0842ee092a28ea53cd4
Joseph Choi <joseph375@revature.net>
2022-07-15 19:21:25 -0400

    Initial implementation of image upload completed.

--- Trailmates/src/app/profile-page/trail-history/trail-history.component.ts
+++ Trailmates/src/app/profile-page/trail-history/trail-history.component.ts
@@ -6,7 +6,6 @@ import { Trail } from 'src/app/models/trail';
 import { User } from 'src/app/models/user'
 import { NgForm } from '@angular/forms';
 import { fade } from '../../animations/animations';
-import { BucketURL } from 'src/app/models/bucketurl';
 // import { Trail } from '../models/trails';
 
 @Component({
@@ -19,9 +18,7 @@ import { BucketURL } from 'src/app/models/bucketurl';
 export class TrailHistoryComponent implements OnInit {
   comment: string = ""
   id: string | null = localStorage.getItem('id')
-  bucketURL: BucketURL = {
-    url: ""
-  }
+  selectedFiles: any = '';
 
   constructor(private trailhistory:TrailHistoryService) { }
 
@@ -62,26 +59,9 @@ displayFormSubmitError: boolean = false
 image: any;
 
 processForm(postForm: NgForm) {
-  //get image file as HTMLInputElement
-  let imageElement = document.getElementById("myFile") as HTMLInputElement;
-  //change input from HTMLInputElement to File
-  let imageFile = imageElement?.files![0];
-  this.image = imageFile;
-  //get secure url to s3 bucket from backend server, passing in the file's extension
-  this.trailhistory.getSecureURL(imageFile.name.split('.').pop()!).then((stringPromise) => {
-    this.bucketURL = stringPromise;
-    console.log("aws URL: " + this.bucketURL.url);
-    //upload the image to S3 bucket
-    this.trailhistory.uploadImage(this.bucketURL.url!, imageFile).subscribe(response => {
-      console.log(response);
-    });
-    //get url to image
-    let imageURL = this.bucketURL.url!.split('?')[0];
-    // console.log(imageURL);
-  })
-
-  // this.trailhistory.uploadImage("https://trailmates-images.s3.amazonaws.com/6a5f86f2-d7aa-4c97-89aa-52af219ac357.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20220715T212057Z&X-Amz-SignedHeaders=host&X-Amz-Expires=600&X-Amz-Credential=AKIATSTKADK3AQ7PYDNZ%2F20220715%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=7ed1a5b6dc5dfe1cfdadaa726872039faf01735c6d0f5a6d520a8ae27121f814", imageFile);
-
+  const imageElement = document.getElementById("myFile") as HTMLInputElement;
+  const imageFile = imageElement.files![0];
+  this.trailhistory.uploadFile(imageFile);
   // this.trailhistory.insertNewHistory(this.historyReq).subscribe((data: any) =>{
   //   console.log(data)
   // })
@@ -120,9 +100,6 @@ close() {
   this.doPassPopup.emit(this.popup);
 } 
 
-
-
-
-} 
+}
 
 
--- Trailmates/src/app/services/trail-history.service.ts
+++ Trailmates/src/app/services/trail-history.service.ts
@@ -5,6 +5,8 @@ import { firstValueFrom, Observable } from 'rxjs';
 import { TrailHistory } from '../models/trailHistory';
 import { BucketURL } from '../models/bucketurl';
 import { HttpHeaders } from '@angular/common/http';
+import * as AWS from 'aws-sdk/global';
+import * as S3 from 'aws-sdk/clients/s3';
 
 @Injectable({
   providedIn: 'root'
@@ -18,10 +20,7 @@ export class TrailHistoryService {
   constructor(private http:HttpClient, private route:Router) { }
 
   headerList = {
-    'Content-Type':'image/*'
-  }
-  requestOptions = {
-    headers: new HttpHeaders(this.headerList),
+    'Content-Type':'image/jpg'
   }
 
   getHistoryDesc(): Observable<TrailHistory[]> {
@@ -36,16 +35,6 @@ export class TrailHistoryService {
     return firstValueFrom(this.http.get<BucketURL>(this.imageURL + "/gen-url/" + fileExtension));
   }
 
-  // uploadImage(bucketURL: String, uploadedImage: File): Observable<any> {
-  //   return this.http.put(bucketURL.toString(), uploadedImage, this.requestOptions);
-  // }
-
-  uploadImage(bucketURL: String, uploadedImage: File) {
-    const formData = new FormData();
-    formData.append('image', uploadedImage);
-    return this.http.post<any>(bucketURL.toString(), formData, this.requestOptions);
-  }
-
   saveImageData(imageData: ImageData) {
     return this.http.post<ImageData>(this.imageURL, imageData)
   }
@@ -53,4 +42,30 @@ export class TrailHistoryService {
   insertNewHistory(newHistory: TrailHistory) {
     return this.http.post<any>(this.URL+'/newHistory', newHistory);
   }
+
+  uploadFile(file: File) {
+    const contentType = file.type;
+    const bucket = new S3(
+          {
+              accessKeyId: 'AKIATSTKADK3JESDFRP2',
+              secretAccessKey: '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',
+              region: 'us-east-1'
+          }
+      );
+      const params = {
+          Bucket: 'trailmates-images',
+          Key: file.name,
+          Body: file,
+          ACL: 'public-read',
+          ContentType: contentType
+      };
+      bucket.upload(params, function (err: any, data: any) {
+          if (err) {
+              console.log('There was an error uploading your file: ', err);
+              return false;
+          }
+          console.log('Successfully uploaded file.', data);
+          return true;
+      });
+}
 }
