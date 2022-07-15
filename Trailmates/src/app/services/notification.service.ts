3f33ce054121f62b425f174aafcfc2f61d3a0d5c
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-07-15 11:11:47 -0500

    Merged with Joseph to resolve history post.

--- Trailmates/src/app/services/notification.service.ts
+++ Trailmates/src/app/services/notification.service.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
 export class NotificationService {
 
   constructor(private http: HttpClient) { }
-  private url = 'https://revature.trailmates.net/TrailMates/notification/'
+  private url = 'http://localhost:8080/TrailMates/notification/'//https://revature.trailmates.net/TrailMates/notification/'
   getAllNotifications(): Observable<Notification[]> {
     return this.http.get<Notification[]>(this.url);
   }
