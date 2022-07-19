2973f08ca7a84c2dac5907b024259df8db210add
HaibunisCool <97471066+HaibunisCool@users.noreply.github.com>
2022-07-19 10:34:51 -0500

    New convos arent made if already exists.

--- Trailmates/src/app/profile-page/friends-list/friends-list.component.ts
+++ Trailmates/src/app/profile-page/friends-list/friends-list.component.ts
@@ -1,3 +1,7 @@
+import { MessagesService } from 'src/app/services/messages.service';
+import { Conversation } from 'src/app/models/conversation';
+import { OwnedCoversation } from 'src/app/models/ownedCoversations';
+import { User } from 'src/app/models/user';
 import { Router } from '@angular/router';
 import { Friend } from 'src/app/models/friend';
 import { Component, OnInit } from '@angular/core';
@@ -7,65 +11,91 @@ import { FriendService } from 'src/app/services/friend.service';
 @Component({
   selector: 'app-friends-list',
   templateUrl: './friends-list.component.html',
-  styleUrls: ['./friends-list.component.scss']
+  styleUrls: ['./friends-list.component.scss'],
 })
 export class FriendsListComponent implements OnInit {
-
-  constructor(private _friendsService: FriendService, private router: Router) { }
-  pendingFriends: Friend[] = []; 
+  constructor(private _friendsService: FriendService, private router: Router, private _messageService: MessagesService) {}
+  pendingFriends: Friend[] = [];
   allFriends: Friend[] = [];
   showFriends: boolean = true;
   showPending: boolean = false;
 
   ngOnInit(): void {
-    this.getFriends()
+    this.getFriends();
 
     this.getPendingFriends();
   }
 
-  getFriends(){
-      this._friendsService.getAllFriends().subscribe(
-      data=>{
+  getFriends() {
+    this._friendsService.getAllFriends().subscribe((data) => {
       this.allFriends = data;
-      }
-    )
+    });
   }
 
-  getPendingFriends(){
-    this._friendsService.getAllPending().subscribe(
-      data=>{
-        this.pendingFriends = data;
-      }
-    )
+  getPendingFriends() {
+    this._friendsService.getAllPending().subscribe((data) => {
+      this.pendingFriends = data;
+    });
   }
 
-  toggle(){
+  toggle() {
     this.showFriends = !this.showFriends;
     this.showPending = !this.showPending;
   }
 
-  goToProfile(username: string){
-    this.router.navigateByUrl('/profile/'+username)
+  goToProfile(username: string) {
+    this.router.navigateByUrl('/profile/' + username);
   }
 
-  goToMessaging(){
-    this.router.navigateByUrl('/messaging');
+  goToMessage(user: User) {
+    let ownedConvo: OwnedCoversation[] = [];
+    let convo: Conversation = {
+      id: '',
+      name: '',
+    };
+    //Gets existing convos
+    this._messageService.getExistingConvos().subscribe((data) => {
+      ownedConvo = data;
+      ownedConvo.forEach((owned) => {
+        //Checks if convo by naming convention exists already
+        if (
+          owned.conversation?.name ==
+            `${user.username!}:${localStorage.getItem('username')}` ||
+          owned.conversation?.name ==
+            `${localStorage.getItem('username')}:${user.username!}`
+        ) {
+          //Sets it equal to convo to compare.
+          convo = owned;
+        }
+      });
+      //If convo id is not still == '', meaning that the convo name did exist already.
+      if (convo.id != '') {
+        this.router.navigateByUrl('messaging/groupchat/' + convo.id);
+        console.log('No new convos made.');
+      } else {
+        //Else creates new convo based on this naming convention.
+        let convoReq = {
+          conversationName:
+            user.username + ':' + localStorage.getItem('username'),
+          userIDs: [user.id],
+        };
+        let convoId = '';
+        this._messageService.createNewGroup(convoReq).subscribe((data) => {
+          convoId = data.id;
+          this.router.navigateByUrl('/messaging/groupchat/' + convoId);
+          console.log('New convo was made.');
+        });
+      }
+    });
   }
 
-  addFriend(id: string){
-    this._friendsService.addFriend(id)
+  addFriend(id: string) {
+    this._friendsService.addFriend(id);
     this.getPendingFriends();
     this.getPendingFriends();
     this.getFriends();
     this.getFriends();
   }
-
-
-
-
-
-
-  
 }
 
 
