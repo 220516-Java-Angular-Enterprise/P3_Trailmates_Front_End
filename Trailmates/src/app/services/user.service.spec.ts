b49226138407534b558a55fcc6cd7e09fb2266c8
Phutondog <ericmphu@gmail.com>
2022-07-20 10:41:52 -0400

    added more test

--- Trailmates/src/app/services/user.service.spec.ts
+++ Trailmates/src/app/services/user.service.spec.ts
@@ -1,19 +1,55 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { UserService } from './user-service.service';
 
 describe('UserServiceService', () => {
   let service: UserService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
     });
     service = TestBed.inject(UserService);
-  });
+    httpTestingController = TestBed.inject(HttpTestingController);
+
+  // public ROOT_URL = 'https://revature.trailmates.net/TrailMates/user/';
+
+  // getUserByUsername(username: string): Observable < User > {
+  //   return this.http.get<User>(this.ROOT_URL + `user-username/${username}`);
+  // }
+
+  // getAllUsers(): Observable < User[] > {
+  //   return this.http.get<User[]>(this.ROOT_URL + "all-users");
+  // }
+
+  // UpdateUser(updatedUser: User): Observable < User > {
+  //   return this.http.put<User>(this.ROOT_URL + 'edit', updatedUser);
+  // }
+  // });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('get user by id'), () => {
+    service.getUserById('fakeid').subscribe();
+
+    const request = httpTestingController.expectOne(data =>
+      data.method === 'GET' && data.url === 'https://revature.trailmates.net/TrailMates/user/user-id/fakeid')
+    
+      request.flush(1);
+  }
+
+  it('get user by username'), () => {
+    service.getUserByUsername('fakeusername').subscribe();
+    const request = httpTestingController.expectOne(data =>
+      data.method === 'GET' && data.url === 'https://revature.trailmates.net/TrailMates/user/user-username/fakeid')
+
+    request.flush(1); 
+  })
+
+
+  
 });
