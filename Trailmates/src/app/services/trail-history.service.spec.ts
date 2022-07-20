104fc55deb9243ae6b70c1f6c791720840a67b0d
Phutondog <ericmphu@gmail.com>
2022-07-20 15:59:53 -0400

    more tests

--- Trailmates/src/app/services/trail-history.service.spec.ts
+++ Trailmates/src/app/services/trail-history.service.spec.ts
@@ -1,5 +1,5 @@
 import { RouterTestingModule } from '@angular/router/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 
@@ -8,15 +8,38 @@ import { TrailHistoryService } from './trail-history.service';
 
 describe('TrailHistoryService', () => {
   let service: TrailHistoryService;
+  let httpTestingController: HttpTestingController;
+  const ROOT_URL = "https://revature.trailmates.net/TrailMates/history";
+  const historyReq = {
+    trail_name: 'FAKE_TRAIL',
+    comment: 'FAKE_COMMENT',
+    date: 'FAKE_DATE',
+    imageURL: 'FAKE_URL',
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule, RouterTestingModule],
     });
     service = TestBed.inject(TrailHistoryService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('shoud get history of user', () => {
+    service.getHistoryAsc('fakeuserid').subscribe();
+    const request = httpTestingController.expectOne(data => 
+      data.method === 'GET' && data.url === ROOT_URL + '/asc/fakeuserid' )
+    request.flush(1);
+  })
+
+  it('shoud insert', () => {
+    service.insertNewHistory(historyReq).subscribe();
+    const request = httpTestingController.expectOne(data =>
+      data.method === 'POST' && data.url === ROOT_URL + '/newHistory')
+    request.flush(1);
+  })
 });
